d5cec7b032452cfd0a4c66c3009eb268ed8fc08a
Kamikaze <timo1322@gmail.com>
2025-04-26 21:09:32 +0200

    Update AutoOffline.plugin.js

--- plugins/AutoOffline/AutoOffline.plugin.js
+++ plugins/AutoOffline/AutoOffline.plugin.js
@@ -1,79 +1,74 @@
 /**
  * @name AutoOffline
- * @version 1.0.0
+ * @version 1.0.1
  * @author Kamiikaze
  * @authorId 141851936168214528
  * @description Automatically sets your Discord status to online on app start and invisible on app quit.
  */
+
+
+const UserSettingsProtoStore = BdApi.Webpack.getStore("UserSettingsProtoStore");
  
- /**
- * The module for accessing user settings.
- * @typedef {Object} UserSettingsProtoStore
- * @property {Object} settings - The user settings object.
- */
-const UserSettingsProtoStore = BdApi.Webpack.getModule(
-	(m) =>
-		m && typeof m.getName == "function" && m.getName() == "UserSettingsProtoStore" && m,
-	{ first: true, searchExports: true }
-);
- 
-/**
- * Utility functions for updating user settings.
- * @typedef {Object} UserSettingsProtoUtils
- * @property {Function} updateAsync - Asynchronously updates a user setting.
- */
 const UserSettingsProtoUtils = BdApi.Webpack.getModule(
 	(m) => m?.ProtoClass?.typeName?.endsWith(".PreloadedUserSettings"),
 	{ first: true, searchExports: true }
 );
- 
+
+
 module.exports = class AutoOffline {
 	constructor(meta) {
 		this.meta = meta;
-		this.currentUserStatus = null
+		this.currentUserStatus = 'invisible'
+		this.listenerAdded = false;
+		this.handleUnload = this.handleUnload.bind(this);
 	}
 	
 	start() {
-		const targetStatus = 'online' 
-		this.currentUserStatus = this.currentStatus()
+		console.log("[AutoOffline] Plugin started");
+		this.currentUserStatus = this.getCurrentStatus();
+		this.updateStatus("online");
 		
-		this.updateStatus('online')
-		
-		// Adding EventListener for App closing
-		window.addEventListener('beforeunload', () => {
-			this.updateStatus('invisible');
-		});
+		if (!this.listenerAdded) {
+			window.addEventListener("beforeunload", this.handleUnload);
+		}
 	}
 
 	stop() {
-		this.updateStatus('invisible')
+		console.log("[AutoOffline] Plugin stopped");
+		
+		if (this.listenerAdded) {
+			window.removeEventListener("beforeunload", this.handleUnload);
+			this.listenerAdded = false;
+		}
 	}
 	
-	currentStatus() {
+	handleUnload() {
+		this.updateStatus("invisible");
+	}
+	
+	getCurrentStatus() {
 		try {
 			return UserSettingsProtoStore.settings.status.status.value;
 		} catch (error) {
 			console.error("ERRORS.ERROR_GETTING_CURRENT_USER_STATUS", error);
-			BdApi.UI.showToast("ERRORS.ERROR_GETTING_CURRENT_USER_STATUS", {type:"error"});
-			return "";
+			return "invisible";
 		}
 	}
 	
-	updateStatus(toStatus) {
-		if (toStatus == this.currentUserStatus) return;
+	updateStatus(newStatus) {
+		if (newStatus == this.currentUserStatus) return;
 			
 		try {
 			UserSettingsProtoUtils.updateAsync(
 				"status",
 				(statusSetting) => {
-					statusSetting.status.value = toStatus;
+					statusSetting.status.value = newStatus;
 				},
 				0
 			);
-			BdApi.UI.showToast("Status changed: " + toStatus, {type:"success"});
+			BdApi.UI.showToast("Status changed: " + newStatus, {type:"success"});
 		} catch (error) {
 			console.error("ERRORS.ERROR_UPDATING_USER_STATUS", error);
-			BdApi.UI.showToast("ERRORS.ERROR_UPDATING_USER_STATUS", {type:"error"});
 		}
 	}
 };
